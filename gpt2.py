891f13e4a2a25aff746021646e371ea61eb24e25
Nils Carlson <nils.carlson@yale.edu>
2020-03-11 13:15:42 -0400

    Foundations

---
+++ gpt2.py
@@ -0,0 +1,32 @@
+import gpt_2_simple as gpt2
+import tensorflow as tf
+
+
+class GPT2():
+    """
+    Generate music using OpenAI's GPT2 text generation
+    """
+
+    def __init__():
+        pass
+
+    def load(self):
+        """
+        Load the model from file
+        """
+        pass
+
+    def train(self, music_text):
+        """
+        Given a string corpus of training music,
+        fine tune the loaded model
+        """
+        pass
+
+    def get_music(self, length=300):
+        """
+        Return a string which can be converted back into music
+        """
+        pass
+
+
